b6334937357f3bf4d7d0e8075cb9f139d6499d23
rocco337 <roko.bobic@gmail.com>
2015-08-13 01:18:24 +0200

    initial commit

---
+++ DynamicSortBy/DynamicSortBy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSortBy
+{
+    public static class DynamicSortBy
+    {
+        public static IEnumerable<T> OrderByDynamic<T>(this IEnumerable<T> list, string propertyName)
+        {
+            return list.AsQueryable().ApplyOrder(propertyName, "OrderBy");
+        }
+
+        public static IEnumerable<T> OrderByDescDynamic<T>(this IEnumerable<T> list, string propertyName)
+        {
+            return list.AsQueryable().ApplyOrder(propertyName, "OrderByDescending");
+        }
+
+        private static IOrderedQueryable<T> ApplyOrder<T>(this IQueryable<T> source, string property, string methodName)
+        {
+            var props = property.Split('.');
+            var type = typeof(T);
+            var arg = Expression.Parameter(type, "x");
+            Expression expr = arg;
+            foreach (var prop in props)
+            {
+                var pi = type.GetProperty(prop);
+                expr = Expression.Property(expr, pi);
+                type = pi.PropertyType;
+            }
+            var delegateType = typeof(Func<,>).MakeGenericType(typeof(T), type);
+            var lambda = Expression.Lambda(delegateType, expr, arg);
+
+            var result = typeof(Queryable).GetMethods().Single(
+                    method => method.Name == methodName
+                            && method.IsGenericMethodDefinition
+                            && method.GetGenericArguments().Length == 2
+                            && method.GetParameters().Length == 2)
+                    .MakeGenericMethod(typeof(T), type)
+                    .Invoke(null, new object[] { source, lambda });
+
+            return (IOrderedQueryable<T>)result;
+        }
+    }
+}
---
+++ DynamicSortBy/DynamicSortTest.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSortBy
+{
+    [TestFixture]
+    public class DynamicSortTest
+    {
+        private List<EmptyClass> randomValues = new List<EmptyClass>();
+
+        public void Setup(int itemCount)
+        {
+            var rnd = new Random();
+            for (var ii = 0; ii < itemCount; ii++)
+            {
+                randomValues.Add(new EmptyClass() { Value = rnd.Next() });
+            }
+
+            rnd = new Random();
+            randomValues = randomValues.OrderBy(item => rnd.Next()).ToList();
+        }
+
+        [TestCase(100000,"asc")]
+        [TestCase(100000, "desc")]            
+        public void DynamicSort(int itemCount,string direction)
+        {
+            Setup(itemCount);
+
+            var sw = Stopwatch.StartNew();
+
+            if (direction == "asc")
+            {
+                randomValues = randomValues.OrderByDynamic("Value").ToList();
+            }
+            else
+            {
+                randomValues = randomValues.OrderByDescDynamic("Value").ToList();
+            }
+
+            sw.Stop();
+
+            var sw1 = Stopwatch.StartNew();
+
+            if (direction == "asc")
+            {
+                randomValues = randomValues.OrderBy(m => m.Value).ToList();
+            }
+            else
+            {
+                randomValues = randomValues.OrderByDescending(m => m.Value).ToList();
+            }
+
+            sw.Stop();
+
+            //just check part of collection
+            for (var ii = 0; ii <= itemCount-1; ii++)
+            {
+                if (direction == "asc")
+                {
+                    Assert.LessOrEqual(randomValues[ii].Value, randomValues[++ii].Value);
+                }
+                else
+                {
+                    Assert.GreaterOrEqual(randomValues[ii].Value, randomValues[++ii].Value);
+                }
+                
+            }
+
+            Assert.Pass(string.Format("Elapsed dyn: {0} ms, Elapsedregular: {1} ms", sw.ElapsedMilliseconds, sw1.ElapsedMilliseconds));
+
+        }
+
+      
+        class EmptyClass
+        {
+            public int Value { get; set; }
+        }
+    }
+}
